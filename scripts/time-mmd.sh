f3fa761662cda2bc1b5d498eb171949c78dbcd84
gaussriemann <44453909+gaussriemann@users.noreply.github.com>
2025-09-20 17:01:10 +0800

    Update time-mmd.sh for the ChatTime model.

--- scripts/time-mmd.sh
+++ scripts/time-mmd.sh
@@ -27,3 +27,18 @@ python main.py --model_name ts_adapter/unica --base_model timesfm_2_500m --with_
 python main.py --model_name ts_adapter/unica --base_model timesfm_2_500m --with_gate --with_future --future_with_gate --datasets time-mmd/SocialGood --num_batches_per_epoch 50 --max_epochs 100 --indexed_sample --split_val --gradient_clip 0.1 --batch_size 64 --d_multi_modal 1 --lr 1e-5
 # Traffic
 python main.py --model_name ts_adapter/unica --base_model timesfm_2_500m --with_gate --with_future --future_with_gate --datasets time-mmd/Traffic --num_batches_per_epoch 50 --max_epochs 100 --indexed_sample --split_val --gradient_clip 0.1 --batch_size 64 --d_multi_modal 1 --lr 1e-5
+
+# Climate
+python main.py --model_name chattime --with_gate --with_future --future_with_gate --datasets time-mmd/Climate --num_workers 0 --num_batches_per_epoch 20 --indexed_sample --split_val --gradient_clip 1.0 --batch_size 32 --encoder_path chattime
+# Energy
+python main.py --model_name chattime --with_gate --with_future --future_with_gate --datasets time-mmd/Energy --num_workers 0 --num_batches_per_epoch 50 --indexed_sample --split_val --gradient_clip 1.0 --batch_size 32 --encoder_path chattime
+# Environment
+python main.py --model_name chattime --with_gate --with_future --future_with_gate --datasets time-mmd/Environment --num_workers 0 --num_batches_per_epoch 50 --indexed_sample --split_val --gradient_clip 1.0 --batch_size 32 --encoder_path chattime
+# Public_Health
+python main.py --model_name chattime --with_gate --with_future --future_with_gate --datasets time-mmd/Public_Health --num_workers 0 --num_batches_per_epoch 50 --indexed_sample --split_val --gradient_clip 1.0 --batch_size 32 --encoder_path chattime
+# Security
+python main.py --model_name chattime --with_gate --with_future --future_with_gate --datasets time-mmd/Security --num_workers 0 --num_batches_per_epoch 20 --indexed_sample --split_val --gradient_clip 1.0 --batch_size 32 --encoder_path chattime
+# SocialGood
+python main.py --model_name chattime --with_gate --with_future --future_with_gate --datasets time-mmd/SocialGood --num_workers 0 --num_batches_per_epoch 20 --indexed_sample --split_val --gradient_clip 1.0 --batch_size 32 --encoder_path chattime
+# Traffic
+python main.py --model_name chattime --with_gate --with_future --future_with_gate --datasets time-mmd/Traffic --num_workers 0 --num_batches_per_epoch 20 --indexed_sample --split_val --gradient_clip 1.0 --batch_size 32 --encoder_path chattime
